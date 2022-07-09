0ed4d504f4314a0235c0059255b9d7393ce54d0b
khanhdd1121 <102730911+khanhdd1121@users.noreply.github.com>
2022-07-09 17:42:26 +0700

    new repair

---
+++ src/Redux/Api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import {loginStart, loginSuccess, loginFailed} from './AuthSlice'
+
+export  const loginUser = async(user, dispacth, navigation) =>{
+    dispacth(loginStart()); 
+    try {
+        const res = await axios.post('http://18.141.199.110:3000/account-user/login-user', user)
+        dispacth(loginSuccess(res.data))
+        navigation('/shop/tong_quan')
+    } catch (err) {
+        dispacth(loginFailed())
+    }
+}
